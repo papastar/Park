423d544cc2506d58c034f6d88de6d5064b7ba088
papa <papastarpapa@gmail.com>
2016-11-22 20:02:21 +0800

    添加数据库功能

--- app/src/main/java/com/papa/park/app/App.java
+++ app/src/main/java/com/papa/park/app/App.java
@@ -4,8 +4,10 @@ import android.app.Application;
 import android.content.Context;
 
 import com.papa.libcommon.util.AppUtils;
+import com.papa.libcommon.util.Logger;
 import com.papa.libcommon.util.SharedPreferencesUtil;
 import com.papa.libcommon.util.ToastUtils;
+import com.papa.park.BuildConfig;
 
 /**
  * User: PAPA
@@ -24,6 +26,7 @@ public class App extends Application {
     private void init() {
         AppUtils.init(this);
         ToastUtils.init(this);
+        Logger.init("park", BuildConfig.DEV_MODE);
         SharedPreferencesUtil.init(this, getPackageName(), Context.MODE_PRIVATE);
 
     }
---
+++ app/src/main/java/com/papa/park/ui/activity/LockersActivity.java
@@ -0,0 +1,17 @@
+package com.papa.park.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.papa.park.R;
+
+public class LockersActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lockers);
+    }
+}
